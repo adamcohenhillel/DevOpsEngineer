9efe82bd7969a099e3b5e52827743dd069b147ee
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2024-05-05 16:00:06 -0700

    finished

--- agent/agent.py
+++ agent/agent.py
@@ -4,6 +4,8 @@ import subprocess
 import traceback
 
 import openai
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 client = openai.OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
@@ -19,75 +21,108 @@ def read_file(file_path: str) -> str:
         return f.read()
 
 
-def rewrite_file(file_path: str, content: str) -> None:
+def write_file(file_path: str, content: str) -> None:
+    folder = os.path.dirname(file_path)
+    if folder:
+        os.makedirs(folder, exist_ok=True)
     user_int = input(f'About to write to {file_path}:\n {
                      content} \nPress enter to continue or "No" to skip...')
     if user_int == "No":
-        return None
+        return "Skipped writing to file!"
     with open(file_path, 'w') as f:
         f.write(content)
+    return f"Written to {file_path}!"
+
+
+def cd_to_path(path: str) -> str:
+    try:
+        os.chdir(path)
+        return f"Changed directory to {path}"
+    except Exception as e:
+        traceback_str = traceback.format_exc()
+        return traceback_str
+
+
+def make_dir(directory: str) -> str:
+    try:
+        os.makedirs(directory, exist_ok=True)
+        return f"Directory {directory} created!"
+    except Exception as e:
+        traceback_str = traceback.format_exc()
+        return traceback_str
 
 
 def run_awscli_command(command: str) -> str:
     try:
         cmd = command.split(" ")
+        if cmd[0] != "aws":
+            cmd = ["aws"] + cmd
         output = subprocess.check_output(cmd).decode()
         return output
     except Exception as e:
-        return str(e)
+        tarceback_str = traceback.format_exc()
+        return tarceback_str
 
 
-def edit_main_tf(code: str) -> None:
-    os.makedirs("terraform", exist_ok=True)
-    with open("terraform/main.tf", "w") as f:
+def edit_main_tf(maintf_folder: str, code: str) -> None:
+    with open(maintf_folder + "/main.tf", "w") as f:
         f.write(code)
 
+    return f"{maintf_folder + "/main.tf"} file updated!"
 
-def terraform_init(folder: str = "./terraform") -> None:
+
+def terraform_init() -> str:
     try:
-        os.chdir(folder)
-        cmd = ["terraform", "init"]
-        subprocess.run(cmd)
-        os.chdir("..")
-        return "Terraform initialized!"
-    except Exception as e:
-        print("************"*10)
-        print(e)
-        print("************"*10)
-        return str(e)
+        result = subprocess.run(
+            ["terraform", "init"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+        try:
+            # generate graph from the terraform (terraform graph | dot -Tpng > graph.png)
+            subprocess.run(
+                ["terraform", "graph", "|", "dot", "-Tpng", ">", "graph.png"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+            # show graph png:
+            plt.imshow(mpimg.imread('graph.png'))
+            plt.show()
+        except Exception as e:
+            print("Error generating graph: ", e)
+            print("Skipping graph generation...")
+
+        return result.stdout + " / " + result.stdout
+    except Exception:
+        # Capture and return traceback if an exception occurs
+        traceback_str = traceback.format_exc()
+        return traceback_str
 
 
-def terraform_plan(folder: str = "./terraform") -> str:
+def terraform_plan() -> str:
     try:
-        os.chdir(folder)
-        cmd = ["terraform", "plan"]
-        output = subprocess.check_output(cmd).decode()
-        os.chdir("..")
-        return output
-    except Exception as e:
-        return str(e)
+        result = subprocess.run(
+            ["terraform", "plan"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        return result.stdout + " / " + result.stdout
+    except Exception:
+        traceback_str = traceback.format_exc()
+        return traceback_str
 
 
-def terraform_apply(folder: str = "./terraform") -> None:
+def terraform_apply() -> str:
     try:
-        os.chdir(folder)
-        cmd = ["terraform", "apply", "-auto-approve"]
-        subprocess.run(cmd)
-        os.chdir("..")
-        return "Terraform applied!"
-    except Exception as e:
-        return str(e)
+        result = subprocess.run(["terraform", "apply", "-auto-approve"],
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        return result.stdout + " / " + result.stdout
+    except Exception:
+        traceback_str = traceback.format_exc()
+        return traceback_str
 
 
-def terraform_destroy(folder: str = "./terraform") -> None:
+def terraform_destroy() -> str:
     try:
-        os.chdir(folder)
-        cmd = ["terraform", "destroy", "-auto-approve"]
-        subprocess.run(cmd)
-        os.chdir("..")
-        return "Terraform destroyed!"
-    except Exception as e:
-        return str(e)
+        result = subprocess.run(["terraform", "destroy", "-auto-approve"],
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        return result.stdout + " / " + result.stdout
+    except Exception:
+        traceback_str = traceback.format_exc()
+        return traceback_str
 
 
 def ask_input_from_user(prompt: str) -> str:
@@ -96,17 +131,31 @@ def ask_input_from_user(prompt: str) -> str:
 
 def run_agent(task: str, path: str) -> None:
 
-    sys_prompt = """You are an AI agent that function as a highly skilled DevOps engineer with a specialization in using Terraform for infrastructure as code and deploying applications on Amazon Web Services (AWS).
+    sys_prompt = f"""You are an AI agent that function as a highly skilled DevOps engineer with a specialization in using Terraform for infrastructure as code and deploying applications on Amazon Web Services (AWS).
 The agent possesses deep knowledge of cloud architectures, security best practices, and efficient resource management.
 
 Do not ask the user for input unless it is extremely necessary. You, as highly skilled DevOps, should make decisions by yourself and only ask the user for input when it is absolutely required.
 
-You can use AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables to authenticate to AWS.
+Current PWD: {os.getcwd()}
 
 RULES:
 1. You must use Terraform for managing resoucres. Only use AWS CLI to list and check resources.
+2. Always use region us-east-1
+3. Make sure you understand the code you need to work with before executing the given task.
 """
-
+    name_to_function = {
+        "list_files": list_files,
+        "read_file": read_file,
+        "write_file": write_file,
+        "edit_main_tf": edit_main_tf,
+        "terraform_init": terraform_init,
+        "terraform_plan": terraform_plan,
+        "terraform_apply": terraform_apply,
+        "ask_input_from_user": ask_input_from_user,
+        "run_awscli_command": run_awscli_command,
+        "make_dir": make_dir,
+        "cd_to_path": cd_to_path,
+    }
     tools = [
         {
             "type": "function",
@@ -145,7 +194,7 @@ RULES:
         {
             "type": "function",
             "function": {
-                "name": "rewrite_file",
+                "name": "write_file",
                 "description": "Rewrite a file with new content",
                 "parameters": {
                     "type": "object",
@@ -161,23 +210,6 @@ RULES:
                     },
                     "required": ["file_path", "content"],
                 },
-            },
-        },
-        {
-            "type": "function",
-            "function": {
-                "name": "write_main_tf",
-                "description": "Write code into main.tf file in terraform directory",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "code": {
-                            "type": "string",
-                            "description": "The code to write to main.tf",
-                        },
-                    },
-                },
-                "required": ["code"],
             },
         },
         {
@@ -234,12 +266,47 @@ RULES:
                 },
                 "required": ["command"],
             },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "make_dir",
+                "description": "Create a directory",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "directory": {
+                            "type": "string",
+                            "description": "The directory to create",
+                        },
+                    },
+                    "required": ["directory"],
+                },
+            },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "cd_to_path",
+                "description": "Change directory to the specified path",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "path": {
+                            "type": "string",
+                            "description": "The path to change to",
+                        },
+                    },
+                    "required": ["path"],
+                },
+            },
         }
     ]
 
     messages = [
         {"role": "system", "content": sys_prompt},
-        {"role": "user", "content": f"Your task is: {task} in project {path}"},
+        {"role": "user", "content": f"Your task is: {
+            task}. You should create whatever resources are needed to accomplish this task."},
     ]
 
     running = True
@@ -248,7 +315,7 @@ RULES:
         print('\33[32m\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[ Agent Loop ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\33[0m')
         try:
             completion = client.chat.completions.create(
-                model="gpt-4-0125-preview",
+                model="gpt-4-turbo",
                 messages=messages,
                 tools=tools,
             )
@@ -277,58 +344,23 @@ RULES:
             for tool in new_message.tool_calls:
                 print(f'\33[36mAgent is executing tool: {
                       tool.function.name}\33[0m')
-                print(f'\33[36margs: {tool.function.arguments}\33[0m')
-                args = json.loads(tool.function.arguments)
-
-                if tool.function.name == "list_files":
-                    result = list_files(args['folder'])
+                print(f'\33[36mkwargs: {tool.function.arguments}\33[0m')
+                kwargs = json.loads(tool.function.arguments)
+                function_to_call = name_to_function.get(tool.function.name)
+                if function_to_call:
+                    result = function_to_call(**kwargs)
                     messages.append(
                         {"role": "tool", "tool_call_id": tool.id, "content": str(result)})
-
-                elif tool.function.name == "read_file":
-                    result = read_file(args['file_path'])
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
-                elif tool.function.name == "rewrite_file":
-                    rewrite_file(args['file_path'], args['content'])
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": "File rewritten!"})
-
-                elif tool.function.name == "write_main_tf":
-                    edit_main_tf(args['code'])
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": "Code written to main.tf!"})
-
-                elif tool.function.name == "ask_input_from_user":
-                    result = ask_input_from_user(args['prompt'])
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
-                elif tool.function.name == "terraform_init":
-                    result = terraform_init()
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
-                elif tool.function.name == "terraform_plan":
-                    result = terraform_plan()
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
-                elif tool.function.name == "terraform_apply":
-                    terraform_apply()
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
-                elif tool.function.name == "run_awscli_command":
-                    result = run_awscli_command(args['command'])
-                    messages.append(
-                        {"role": "tool", "tool_call_id": tool.id, "content": result})
-
                 else:
-                    print("FUCCCCK")
+                    messages.append(
+                        {"role": "tool", "tool_call_id": tool.id, "content": "Tool not found!"})
 
+                print("***"*10)
+                print(f'\33[33m\nResult:\n{messages[-1]}\33[0m')
+                print("----")
+                print("----")
                 print(f'\33[33m\nResult:\n{messages[-1]["content"]}\33[0m')
+                print("***"*10)
                 print("----")
 
         except Exception as e:
